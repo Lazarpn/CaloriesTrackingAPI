824655d24b13a5703086eee75b7a759707e02382
Lazar <lazarst.pn@gmail.com>
2023-05-20 08:25:19 +0200

    Versioning implemented

--- Program.cs
+++ Program.cs
@@ -14,6 +14,9 @@ using CaloriesTrackingAPI.Context;
 using CaloriesTrackingAPI.Models.Users;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.Net.Http.Headers;
 
 namespace HotelListingApi;
 
@@ -74,6 +77,31 @@ public class Program
             };
         });
 
+        builder.Services.AddApiVersioning(options =>
+        {
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.DefaultApiVersion = new ApiVersion(1, 0);
+            options.ReportApiVersions = true;
+            options.ApiVersionReader =  ApiVersionReader.Combine(
+                new QueryStringApiVersionReader("api-version"), 
+                new HeaderApiVersionReader("X-Version"), 
+                new MediaTypeApiVersionReader("ver"));
+        });
+
+        builder.Services.AddVersionedApiExplorer(
+            options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+
+        builder.Services.AddResponseCaching(options =>
+        {
+            options.MaximumBodySize = 1024;
+            options.UseCaseSensitivePaths = true;
+        });
+
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
@@ -89,6 +117,21 @@ public class Program
         app.UseHttpsRedirection();
 
         app.UseCors("AllowAll");
+
+        app.UseResponseCaching();
+
+        app.Use(async (context, next) =>
+        {
+            context.Response.GetTypedHeaders().CacheControl =
+                new Microsoft.Net.Http.Headers.CacheControlHeaderValue()
+                {
+                    Public = true,
+                    MaxAge = TimeSpan.FromSeconds(10),
+                };
+            context.Response.Headers[HeaderNames.Vary] =
+                new string[] { "Accept-Encoding" };
+            await next();
+        });
 
 
         app.UseAuthentication();
